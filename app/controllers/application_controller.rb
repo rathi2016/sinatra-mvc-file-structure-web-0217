a10632668a5577fded371bce48820e1046d3b218
github username <github email address>
2017-02-20 11:31:06 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,3 +9,18 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 end
+  class Dog
+     attr_accessor :name ,:age,:breed
+
+     DOGS = []
+    def initialize(name ,breed,age)
+      @name = name
+      @age = age
+      @breed = breed
+      DOGS << self
+    end
+
+    def self.all
+    DOGS
+    end
+  end
